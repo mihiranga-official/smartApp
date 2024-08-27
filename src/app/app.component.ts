1a15b5c88e99bb6d892294175ef1a9b6948ce18b
janith <mihiranga.office@gmail.com>
2024-08-27 23:17:00 +0530

    Responsive web page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ResponsiveComponent } from "./responsive/responsive.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ResponsiveComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
